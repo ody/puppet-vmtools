bf8e7358a01b503155e00c198397ba503971dd46
Cody Herriges <c.a.herriges@gmail.com>
2010-09-03 12:20:41 -0700

    Comments and classing.

--- localrepo.pp
+++ localrepo.pp
@@ -1,4 +1,29 @@
-# Author: 
+# Author: Cody Herriges
+# Pulls a selection of packages from a full Centos 5 mirror and
+# drops the packages into a requested location on the local machine
+# if any packages are updated it then runs createrepo to generate
+# a local yum repo.  The local repos are meant to allow PuppetMaster
+# trainings to be ran in the event that internet connectivity is an
+# issue.
+#
+# All package patterns in each local repo need to currently be with in the
+# same resource.  This is due to the method of retrieving and cleaning
+# up packages; each resource delaration is going to issues a `rsync
+# --delete` with means that you will only get package from the final
+# resource that runs.  Suboptimal, yes and I think I am going to solve
+# this with a ruby manifest at some point.
+#
+# Example:
+#   pkgsync { "base_pkgs":
+#     pkglist  => "httpd*\nperl-DBI*\nlibart_lgpl*\napr*\nruby-rdoc*\nntp*\n",
+#     repopath => "/var/yum/mirror/centos/5/os/i386",
+#     source   => "::centos/5/os/i386/CentOS/",
+#     notify   => Repobuild["base"]
+#   }
+#
+#   repobuild { "base":
+#     repopath => "${base}/mirror/centos/5/os/i386",
+#   }
 
 define pkgsync ($pkglist = $name, $source, $server = "mirrors.cat.pdx.edu", $syncer = "rsync", $syncops = "-rltDvzPH --delete --delete-after", $repopath) {
 
@@ -22,7 +47,7 @@ define pkgsync ($pkglist = $name, $source, $server = "mirrors.cat.pdx.edu", $syn
 
 
 define repobuild ($repopath, $repoer = "createrepo", $repoops = "-C --update -p") {
- 
+
   exec { "${name}_build":
     command     => "${repoer} ${repoops} ${repopath}",
     user        => puppet,
@@ -31,7 +56,10 @@ define repobuild ($repopath, $repoer = "createrepo", $repoops = "-C --update -p"
     refreshonly => true,
   }
 
-} 
+}
+
+class localpm {
+
 $base = "/var/yum"
 
 $directories = [ "${base}",
@@ -72,6 +100,8 @@ pkgsync { "base_pkgs":
 
 repobuild { "base":
   repopath => "${base}/mirror/centos/5/os/i386",
+}
+
 }
 
 #exec {
